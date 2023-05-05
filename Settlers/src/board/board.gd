dc3853cde60a6349c74aed7489e16f02f0c3913a
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2023-05-05 13:55:08 +0200

    Added data

--- Settlers/src/board/board.gd
+++ Settlers/src/board/board.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+var Tile: = preload("res://src/board/tile.tscn")
+var recources_dict: = Resources.new()
+
 func _ready() -> void:
 	$HexGrid.build(2)
+	randomize_board()
+
+func randomize_board() -> void:
+	var tiles: Array[int] = []
+	
+	for cell in $HexGrid.get_cells():
+		var tile: = Tile.instantiate()
+		tile.position = cell.position
+		$Tiles.add_child(tile)
