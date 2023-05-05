19da0813e7bd7e96576e4dcb29fb18993fcc89e2
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2023-05-05 14:16:47 +0200

    Added randomization

--- Settlers/src/game/game.gd
+++ Settlers/src/game/game.gd
@@ -3,3 +3,12 @@ extends Node2D
 func _ready() -> void:
 	$Board.position = get_viewport().get_visible_rect().size * 0.5
 	$GameCamera2D.position = get_viewport().get_visible_rect().size * 0.5
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		reset()
+
+func reset() -> void:
+	$GameState.reset()
+	$UI.reset()
+	$Board.randomize_board()
