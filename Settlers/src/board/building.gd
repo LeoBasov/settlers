e26bf48a830d316d09273ae5cc05651edaa09a56
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2023-05-05 18:12:21 +0200

    Implemented distance check

--- Settlers/src/board/building.gd
+++ Settlers/src/board/building.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var dist: float  = 110.0
+
 @onready var availible: bool = true
 @onready var build: bool = false
 @onready var owning_palyer: int = 0
@@ -7,6 +9,7 @@ extends Node2D
 func _physics_process(delta: float) -> void:
 	if not build and Input.is_action_just_pressed("left_click") and $BuildingSprite.visible:
 		build = true
+		get_tree().call_group("buildings", "check_neighbours", position)
 
 func change_player(owning_palyer_: int) -> void:
 	if not build:
@@ -22,3 +25,8 @@ func _on_building_area_2d_mouse_entered() -> void:
 func _on_building_area_2d_mouse_exited() -> void:
 	if not build:
 		$BuildingSprite.hide()
+
+func check_neighbours(pos: Vector2) -> void:
+	if not build and availible:
+		if (pos - position).length() < dist:
+			availible = false
