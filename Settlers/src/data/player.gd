26d6aa2905c2a23f93d041f124add9f790dcf122
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2023-05-05 12:22:05 +0200

    Update player.gd

--- Settlers/src/data/player.gd
+++ Settlers/src/data/player.gd
@@ -1,3 +1,9 @@
 extends Node
 
 @export var color: Color = Color.RED
+var resources_dict: = Resources.new()
+var resources: Array = []
+
+func _ready() -> void:
+	resources.resize(5)
+	resources.fill(0)
